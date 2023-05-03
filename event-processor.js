d5b0ccea864de7b4b2d5d6ec6421f78b1d07c1c0
Zoltan Merges <zoltanmerges@Zoltans-MacBook-Pro.local>
2023-05-03 23:16:56 +0200

    Add mysql event processing

--- event-processor.js
+++ event-processor.js
@@ -1,8 +1,13 @@
 const kafka = require('kafka-node');
+const mysql = require('mysql');
 const request = require('request');
+
+var MySQLEvents = require('mysql-events');
 
 const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
 const consumer = new kafka.Consumer(client, [{ topic: 'firstTestTopic' }]);
+
+
 
 function sendToExternalEndpoint(data) {
   return new Promise((resolve, reject) => {
@@ -35,3 +40,40 @@ consumer.on('message', function(message) {
       console.error('Error sending data to external endpoint:', error);
     });
 });
+
+
+/**
+ * Listening to row insert
+ */
+const config = {
+  host: 'localhost',
+  user: 'root',
+  password: 'rootpassword',
+  database: 'event_processor',
+  port: 3346,
+};
+
+var myCon = MySQLEvents(config);
+
+myCon.add(
+  'event_processor.payments',
+  function (oldRow, newRow, event) {
+     //row inserted
+    if (oldRow === null) {
+      console.log('row was inserted', newRow);
+    }
+
+     //row deleted
+    if (newRow === null) {
+      //delete code goes here
+    }
+
+     //row updated
+    if (oldRow !== null && newRow !== null) {
+      //update code goes here
+    }
+
+    //detailed event information
+    //console.log(event)
+  }
+);
